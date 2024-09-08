c483cfb1e53701d57e503a028835ca5deee3bdf6
hyunseong <hyunseong@postech.ac.kr>
2024-09-09 00:46:41 +0900

    어싸인 1 초본

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -2,27 +2,41 @@ package recfun
 import common._
 
 object Main {
-  def main(args: Array[String]) {
-    println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
+    def main(args: Array[String]) = {
+        println("Pascal's Triangle")
+        for (row <- 0 to 10) {
+            for (col <- 0 to row) print(pascal(col, row) + " ")
+            println()
+        }
     }
-  }
 
-  /**
-   * Exercise 1
-   */
-  def pascal(c: Int, r: Int): Int = ???
+    /**
+     * Exercise 1
+     */
+    def pascal(c: Int, r: Int): Int = c*r match {
+        case 0 => 1
+        case _ => pascal(c,r-1) + pascal(c-1,r-1)
+    }
 
-  /**
-   * Exercise 2
-   */
-  def balance(chars: List[Char]): Boolean = ???
+    /**
+     * Exercise 2
+     */
+    def balance(chars: List[Char]): Boolean = {
+        def innerRecursive(chars: List[Char], openedParenthesesCount: Int): Boolean = chars match {
+        case Nil => openedParenthesesCount == 0
+        case '(' :> tail => innerRecursive(tail, openedParenthesesCount + 1)
+        case ')' :> tail => openedParenthesesCount > 0 && innerRecursive(tail, openedParenthesesCount - 1)
+        case _ :> tail => innerRecursive(tail, openedParenthesesCount)
+        }
+        innerRecursive(chars, 0)
+    }
 
-  /**
-   * Exercise 3
-   */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+
+    /**
+     * Exercise 3
+     */
+    def countChange(money: Int, coins: List[Int]): Int =
+        if (money == 0) 1
+        else if (money < 0 || coins.isEmpty) 0
+        else countChange(money - coins.head, coins) + countChange(money, coins.tail)
 }
