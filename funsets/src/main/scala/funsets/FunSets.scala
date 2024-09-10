a774306ecd695d3e997b05c1019fe1e617409773
hyunseong <hyunseong@postech.ac.kr>
2024-09-10 23:22:43 +0900

    어싸인 2 초본(버그 체크 안함)

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -20,30 +20,30 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): Set = x => x == elem
+  def singletonSet(elem: Int): Set = {x => x == elem}
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: Set, t: Set): Set = x => s(x) || t(x)
+  def union(s: Set, t: Set): Set = {x => s(x) || t(x)}
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: Set, t: Set): Set = x => s(x) && t(x)
+  def intersect(s: Set, t: Set): Set = {x => s(x) && t(x)}
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = x => s(x) && !t(x)
+  def diff(s: Set, t: Set): Set = {x => s(x) && !t(x)}
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: Set, p: Int => Boolean): Set = x => p(x)
+  def filter(s: Set, p: Int => Boolean): Set = {x => p(x)}
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -53,13 +53,13 @@ object FunSets {
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
-  def forall(s: Set, p: Int => Boolean): Boolean = { // TODO: 모든 bounded integer 반환하는데 왜 반환값이 Boolean???
+  def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a>bound) true
+      else if (contains(s, a) && !p(a)) false
+      else iter(a+1)
     }
-    iter(???)
+    iter(-bound)
   }
 
   /**
@@ -78,7 +78,7 @@ object FunSets {
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: Set, f: Int => Int): Set = x => s(f(x))
+  def map(s: Set, f: Int => Int): Set = {x => s(f(x))}
 
   /**
    * Displays the contents of a set
