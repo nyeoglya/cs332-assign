66681de4d07dcb078479ba1c1541ab71d8cd1713
hyunseong <hyunseong@postech.ac.kr>
2024-09-10 14:09:19 +0900

    어싸인 1 pascal 함수 r == 0 케이스 업데이트

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,7 +14,7 @@ object Main {
      * Exercise 1
      */
     def pascal(c: Int, r: Int): Int = {
-        if (c == 0 || c == r) 1
+        if (c*r == 0 || c == r) 1
         else pascal(c, r-1) + pascal(c-1, r-1)
     }
 
