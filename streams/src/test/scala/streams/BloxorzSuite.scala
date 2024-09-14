091da45daeb8fae1554b0e1bd40159fa847a37f7
hyunseong <hyunseong@postech.ac.kr>
2024-09-14 23:18:14 +0900

    어싸인 6 newNeighborsOnly 테스트 케이스 추가

--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -53,6 +53,22 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("avoiding circles") {
+    new Level1 {
+      assert(
+        newNeighborsOnly(
+          Set(
+            (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+            (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+          ).toStream,
+          Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+        ) == Set(
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        ).toStream
+      )
+    }
+  }
+
   test("optimal solution for level 1") {
     new Level1 {
       assert(solve(solution) == Block(goal, goal))
