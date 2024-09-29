da6dc0bc28887367c8d57ab51ac168394e40eb8f
hyunseong <hyunseong@postech.ac.kr>
2024-09-29 16:07:49 +0900

    어싸인 3 TweetReader 불필요한 코드 삭제

--- objsets/src/main/scala/objsets/TweetReader.scala
+++ objsets/src/main/scala/objsets/TweetReader.scala
@@ -54,17 +54,6 @@ object TweetReader {
   
   private val sources: List[List[objsets.Tweet]] = List(gizmodoTweets, techCrunchTweets, engadgetTweets, amazondealsTweets, cnetTweets, gadgetlabTweets, mashableTweets)
 
-  /*
-  val tweetMap: Map[String, List[Tweet]] =
-    Map() ++ Seq((sites(0) -> gizmodoTweets),
-                 (sites(1) -> techCrunchTweets),
-                 (sites(2) -> engadgetTweets),
-                 (sites(3) -> amazondealsTweets),
-                 (sites(4) -> cnetTweets),
-                 (sites(5) -> gadgetlabTweets),
-                 (sites(6) -> mashableTweets))
-  */
-
   val tweetSets: List[TweetSet] = sources.map(tweets => toTweetSet(tweets))
   
   private val siteTweetSetMap: Map[String, TweetSet] =
@@ -75,8 +64,4 @@ object TweetReader {
     else unionOfAllTweetSets(curSets.tail, acc.union(curSets.head))
 
   val allTweets: TweetSet = unionOfAllTweetSets(tweetSets, new Empty)
-  /*{
-    sources.foreach(subList => println(subList.size))
-    toTweetSet(gizmodoTweets)
-  } */
 }
