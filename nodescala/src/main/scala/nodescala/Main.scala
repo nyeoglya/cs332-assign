d1d139539ce3c271c120f8ecd9d8a89fa4377641
hyunseong <hyunseong@postech.ac.kr>
2024-11-06 20:04:35 +0900

    어싸인 6 Main.scala 내용 완성

--- nodescala/src/main/scala/nodescala/Main.scala
+++ nodescala/src/main/scala/nodescala/Main.scala
@@ -3,12 +3,12 @@ package nodescala
 import scala.language.postfixOps
 import scala.concurrent._
 import scala.concurrent.duration._
+import scala.util._
 import ExecutionContext.Implicits.global
 import scala.async.Async.{async, await}
 
 object Main {
-
-  def main(args: Array[String]) {
+  def main(args: Array[String]) = {
     // 1. instantiate the server at 8191, relative path "/test",
     //    and have the response return headers of the request
     val myServer = new NodeScala.Default(8191)
@@ -25,18 +25,25 @@ object Main {
     // TO IMPLEMENT
     // 3. create a future that completes after 20 seconds
     //    and continues with a `"Server timeout!"` message
-    val timeOut: Future[String] = ???
+    val timeOut: Future[String] = Future.delay(20.seconds).map(_ => "Server timeout!")
 
     // TO IMPLEMENT
     // 4. create a future that completes when either 20 seconds elapse
     //    or the user enters some text and presses ENTER
-    val terminationRequested: Future[String] = ???
+    val terminationRequested: Future[String] = {
+      val timeoutFuture: Future[String] = Future.delay(20.seconds).map(_ => "Timeout elapsed")
+      val userInputFuture: Future[String] = Future.userInput("Press ENTER to terminate: ")
+      Future.firstCompletedOf(List(timeoutFuture, userInputFuture))
+    }
 
     // TO IMPLEMENT
     // 5. unsubscribe from the server
-    terminationRequested onSuccess {
-      case msg => ???
+    terminationRequested onComplete {
+      case Success(msg) =>
+        println(s"Termination requested: $msg")
+        myServerSubscription.unsubscribe()
+      case Failure(ex) =>
+        println(s"An error occurred while waiting for termination: ${ex.getMessage}")
     }
   }
-
 }
